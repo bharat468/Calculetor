1cc67378abe71847eed75362af2e7bce30600376
bharat <bharatpareek256@gmail.com>
2025-10-06 12:03:23 +0530

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+let number = document.querySelectorAll(".number")
+let ac = document.querySelectorAll(".ac")
+let dec = document.querySelectorAll(".dec")
+let parsent = document.querySelectorAll(".parsent")
+let divaid = document.querySelectorAll(".divaid")
+let multiplay = document.querySelectorAll(".multiplay")
+let less = document.querySelectorAll(".less")
+let plus = document.querySelectorAll(".plus")
+let equl = document.querySelectorAll(".equl")
+let input = document.querySelector("input")
+
+let string = ""
+// let arr = Array.from(number)
+number.forEach((button) => {
+    button.addEventListener("click", () => {
+       number.value
+       console.log(number.textContent);
+       input.value.number
+    })
+})
